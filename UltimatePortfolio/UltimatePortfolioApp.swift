a13d6bc98c91e95e4377b4a0a628aa294a9a53fe
AginS <agins.main@gmail.com>
2021-04-23 12:54:43 +0300

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Vlad Vrublevsky on 23.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
