e30a127c73def533fb8c5d42c7783cef105fb263
AginS <agins.main@gmail.com>
2021-04-25 19:19:44 +0300

    Cleaning up Core Data

---
+++ UltimatePortfolio/Project-CoreDataHelpers.swift
@@ -0,0 +1,77 @@
+//
+//  Project-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Vlad Vrublevsky on 25.04.2021.
+//
+
+import Foundation
+
+extension Project {
+    var projectTitle: String {
+        title ?? "New Project"
+    }
+    
+    var projectDetail: String {
+        detail ?? ""
+    }
+    
+    var projectColor: String {
+        color ?? "Light Blue"
+    }
+    
+    var projectItems: [Item] {
+        let itemsArray = items?.allObjects as? [Item] ?? []
+        return itemsArray.sorted { first, second in
+            
+            /* legacy code
+             if first.completed == false {
+                        if second.completed == true {
+                            return true
+                        }
+                    } else if first.completed == true {
+                        if second.completed == false {
+                            return false
+                        }
+                    }
+             */
+            
+            if (first.completed == false)&&(second.completed == true) {
+                    return true
+                }
+            
+            if (first.completed == true)&&(second.completed == false) {
+                    return false
+                }
+            
+
+            if first.priority > second.priority {
+                return true
+            } else if first.priority < second.priority {
+                return false
+            }
+
+            return first.itemCreationDate < second.itemCreationDate
+        }
+    }
+    
+    var completionAmount: Double {
+        let originalItems = items?.allObjects as? [Item] ?? []
+        guard originalItems.isEmpty == false else { return 0 }
+        
+        let completedItems = originalItems.filter(\.completed)
+        return Double(completedItems.count) / Double(originalItems.count)
+    }
+    
+    static var example: Project {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let project = Project(context: viewContext)
+        project.title = "Example Project"
+        project.detail = "This is an example project"
+        project.closed = true
+        project.creationDate = Date()
+        return project
+    }
+}
